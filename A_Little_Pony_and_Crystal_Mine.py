26aaeb817bba21fa96064a0380707c9cedbf62d8
abdulkadir <toedue17@gmail.com>
2026-03-01 06:40:43 +0300

    march 2026

--- A_Little_Pony_and_Crystal_Mine.py
+++ A_Little_Pony_and_Crystal_Mine.py
@@ -1,6 +1,13 @@
 n = int(input())
 
+# for i in range(n):
+#     diamonds = n - abs(n//2 -i)*2
+#     stars = (n-diamonds) // 2
+#     print ("*"* stars + "D"*diamonds + "*"*stars)
+center = n // 2
+
 for i in range(n):
-    diamonds = n - abs(n//2 -i)*2
-    stars = (n-diamonds) // 2
-    print ("*"* stars + "D"*diamonds + "*"*stars)
+    distance = abs(center - i)
+    diamond = n - 2 * distance
+    stars = distance
+    print("*"*stars+ "D"*diamond + "*"*stars)
