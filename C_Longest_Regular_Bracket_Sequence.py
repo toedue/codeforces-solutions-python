f237061d0cc93c927e81b0063b824608468c47d5
abdulkadir <toedue17@gmail.com>
2026-03-17 21:03:32 +0300

    march 2026

---
+++ C_Longest_Regular_Bracket_Sequence.py
@@ -0,0 +1,32 @@
+s = input()
+
+stack = [-1]
+max_len = 0
+count = 0
+
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append(i)
+
+    else:
+        stack.pop()
+
+        if not stack:
+            stack.append(i)
+        else:
+            length = i - stack[-1]
+
+            if length > max_len:
+                max_len = length
+                count = 1
+            elif length == max_len:
+                count += 1
+
+if max_len == 0:
+    print("0 1")
+else:
+    print(max_len, count)
+
+    
+
+
